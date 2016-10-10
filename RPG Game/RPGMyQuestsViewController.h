cc0352674e75c2d4ce0ba8a6ab4d6be404abed5d
Владислав Крут <pantera@zeouniversity.com>
2016-10-10 18:02:30 +0300

    Add My Quests.

---
+++ RPG Game/RPGMyQuestsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPGMyQuestsViewController.h
+//  RPG Game
+//
+//  Created by Владислав Крут on 10/10/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPGMyQuestsViewController : UITableViewController
+
+@end
