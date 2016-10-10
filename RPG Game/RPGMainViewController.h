da8dac9332ca46978b4b82bd6b56eb6384d64d26
mimir <mimir@zeouniversity.com>
2016-10-10 17:42:45 +0300

    Add Base Project.

---
+++ RPG Game/RPGMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPGMainViewController.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/10/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPGMainViewController : UIViewController
+
+@end
---
+++ RPG Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/10/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
