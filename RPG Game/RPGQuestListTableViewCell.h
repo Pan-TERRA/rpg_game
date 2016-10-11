141eddc77330715694a44f267106d9d4ab3180df
Максим Шульга <sunshine@zeouniversity.com>
2016-10-11 14:52:38 +0300

    Add view for custom cell for table of quests list

---
+++ RPG Game/RPGQuestListTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  RPGQuestListTableViewCell.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/11/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPGQuestListTableViewCell : UITableViewCell
+
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *titleLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UITextView *descriptionTextView;
+@property (nonatomic, assign, readwrite) IBOutlet UIImageView *proofTypeImage;
+@property (nonatomic, assign, readwrite) IBOutlet UIImageView *rewardTypeImage;
+
+@end
---
+++ RPG Game/RPGQuestListTableViewCell.m
@@ -0,0 +1,32 @@
+//
+//  RPGQuestListTableViewCell.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/11/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestListTableViewCell.h"
+
+@interface RPGQuestListTableViewCell()
+
+@end
+
+@implementation RPGQuestListTableViewCell
+
+- (void)awakeFromNib
+{
+  [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+  [super setSelected:selected animated:animated];
+}
+
+- (void)dealloc
+{
+  [super dealloc];
+}
+
+@end
