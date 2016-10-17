9a91acb525d66b4f92c70a8a80d233693c12f247
Максим Шульга <sunshine@zeouniversity.com>
2016-10-17 19:45:42 +0300

    Add RPGQuestListResponse class

---
+++ RPG Game/RPGQuestListResponse.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestListResponse.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/17/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGQuestListResponse : NSObject
+
+@end
---
+++ RPG Game/RPGQuestListResponse.m
@@ -0,0 +1,13 @@
+//
+//  RPGQuestListResponse.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/17/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestListResponse.h"
+
+@implementation RPGQuestListResponse
+
+@end
