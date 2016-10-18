673e9becbfa3d40f5ca2864964730be5aaaf0085
Костянтин Паляничко <vercon@zeouniversity.com>
2016-10-18 19:35:51 +0300

    add category over user defaults

---
+++ RPG Game/NSUserDefaults+RPGSessionInfo.m
@@ -0,0 +1,44 @@
+//
+//  NSUserDefaults+RPGSessionInfo.m
+//  RPG Game
+//
+//  Created by Костянтин Паляничко on 10/18/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "NSUserDefaults+RPGSessionInfo.h"
+#import "UserSessionKeys.h"
+
+@implementation NSUserDefaults (RPGSessionInfo)
+
+- (NSString *)sessionUsername
+{
+  return [self objectForKey:kRPGUserSessionKeyUsername];
+}
+
+- (NSString *)sessionAvatar
+{
+  return [self objectForKey:kRPGUserSessionKeyAvatar];
+}
+
+- (NSString *)sessionToken
+{
+  return [self objectForKey:kRPGUserSessionKeyToken];
+}
+
+- (NSInteger)sessionGold
+{
+  return [self integerForKey:kRPGUserSessionKeyGold];
+}
+
+- (NSInteger)sessionCrystals
+{
+  return [self integerForKey:kRPGUserSessionKeyCrystals];
+}
+
+- (NSArray *)sessionCharacters
+{
+  return [self objectForKey:kRPGUserSessionKeyCharacters];
+}
+
+@end
