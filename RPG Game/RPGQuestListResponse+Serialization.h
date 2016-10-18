43e623ee4ee889df0b830928d490d0f0f02d7794
Максим Шульга <sunshine@zeouniversity.com>
2016-10-18 13:47:38 +0300

    Add QuestListResponse+Serialization class

---
+++ RPG Game/RPGQuestListResponse+Serialization.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestListResponse+Serialization.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/18/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGQuestListResponse_Serialization : NSObject
+
+@end
