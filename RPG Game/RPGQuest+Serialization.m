096b0eac997052d647ba12e885e7cb03105dc2a4
Максим Шульга <sunshine@zeouniversity.com>
2016-10-19 17:20:48 +0300

    Add RPGQuest+Serialization class

---
+++ RPG Game/RPGQuest+Serialization.m
@@ -0,0 +1,13 @@
+//
+//  RPGQuest+Serialization.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/19/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuest+Serialization.h"
+
+@implementation RPGQuest_Serialization
+
+@end
