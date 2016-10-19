6febfd61112d9e78bb7bfe3de4c22fe843c8fe7a
Максим Шульга <sunshine@zeouniversity.com>
2016-10-19 17:20:00 +0300

    Add RPGQuest class

---
+++ RPG Game/RPGQuest.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuest.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/19/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGQuest : NSObject
+
+@end
---
+++ RPG Game/RPGQuest.m
@@ -0,0 +1,13 @@
+//
+//  RPGQuest.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/19/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuest.h"
+
+@implementation RPGQuest
+
+@end
