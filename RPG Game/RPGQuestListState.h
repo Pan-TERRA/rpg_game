27d6b86a96afba9222110638b154bbd1f5bb681c
Максим Шульга <sunshine@zeouniversity.com>
2016-10-19 16:56:41 +0300

    Add RPGQuestListState.h

---
+++ RPG Game/RPGQuestListState.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestListState.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/19/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#ifndef RPGQuestListState_h
+#define RPGQuestListState_h
+
+
+#endif /* RPGQuestListState_h */
