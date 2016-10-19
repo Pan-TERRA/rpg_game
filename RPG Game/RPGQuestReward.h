1a89dd1b52d7e83d86dd385f07f0b20e9c9bed7d
Максим Шульга <sunshine@zeouniversity.com>
2016-10-19 17:42:15 +0300

    Add RPGQuestReward class

---
+++ RPG Game/RPGQuestReward.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestReward.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/19/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGQuestReward : NSObject
+
+@end
