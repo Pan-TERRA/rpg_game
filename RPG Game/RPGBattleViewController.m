78dabfae43fe4b02f0ef23c183708ce856bd0c33
Максим Шульга <sunshine@zeouniversity.com>
2016-10-20 13:38:37 +0300

    RPGBattleViewController fix

--- RPG Game/RPGBattleViewController.m
+++ RPG Game/RPGBattleViewController.m
@@ -9,91 +9,95 @@
 #import "RPGBattleViewController.h"
 #import "RPGBackgroundMusicController.h"
 #import "RPGSFXEngine.h"
+#import "RPGNibNames.h"
 
 @interface RPGBattleViewController ()
 
-@property (assign, nonatomic) IBOutlet UILabel *player1NickName;
-@property (assign, nonatomic) IBOutlet UILabel *player2NickName;
-
-@property (assign, nonatomic) IBOutlet UILabel *player1hp;
-@property (assign, nonatomic) IBOutlet UILabel *player2hp;
-
-@property (assign, nonatomic) IBOutlet UIProgressView *player1hpBar;
-@property (assign, nonatomic) IBOutlet UIProgressView *player2hpBar;
-
-@property (assign, nonatomic) IBOutlet UITextView *battleTextView;
-
-@property (assign, nonatomic) IBOutlet UIButton *spell1Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell2Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell3Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell4Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell5Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell6Button;
-@property (assign, nonatomic) IBOutlet UIButton *spell7Button;
-
-@property (assign, nonatomic) IBOutlet UILabel *timer;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *player1NickName;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *player2NickName;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *player1hp;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *player2hp;
+@property (nonatomic, assign, readwrite) IBOutlet UIProgressView *player1hpBar;
+@property (nonatomic, assign, readwrite) IBOutlet UIProgressView *player2hpBar;
+@property (nonatomic, assign, readwrite) IBOutlet UITextView *battleTextView;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell1Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell2Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell3Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell4Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell5Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell6Button;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *spell7Button;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *timer;
 
 @end
 
 @implementation RPGBattleViewController
 
+#pragma mark - Init
 
-#pragma mark - EventHandling
-
-- (IBAction)back:(id)sender
+- (instancetype)init
 {
-    [[RPGBackgroundMusicController sharedBackgroundMusicController] switchToPeace];
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-#pragma mark - Spell Actions
-- (IBAction)spell1_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:1];
-}
-
-- (IBAction)spell2_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:2];
-}
-
-- (IBAction)spell3_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:3];
-}
-
-- (IBAction)spell4_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:4];
-}
-
-- (IBAction)spell5_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:5];
-}
-
-- (IBAction)spell6_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:6];
-}
-
-- (IBAction)spell7_action:(id)sender
-{
-    [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:7];
+  return [super initWithNibName:kRPGBattleViewController
+                         bundle:nil];
 }
 
 #pragma mark - UIViewController
+
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    [[RPGBackgroundMusicController sharedBackgroundMusicController] switchToBattle];
-    
+  [super viewDidLoad];
+  [[RPGBackgroundMusicController sharedBackgroundMusicController] switchToBattle];
+  
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
+  [super didReceiveMemoryWarning];
 }
 
+#pragma mark - Event Handling
+
+- (IBAction)back:(id)aSender
+{
+  [[RPGBackgroundMusicController sharedBackgroundMusicController] switchToPeace];
+  [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Spell Actions
+
+- (IBAction)spell1_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:1];
+}
+
+- (IBAction)spell2_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:2];
+}
+
+- (IBAction)spell3_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:3];
+}
+
+- (IBAction)spell4_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:4];
+}
+
+- (IBAction)spell5_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:5];
+}
+
+- (IBAction)spell6_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:6];
+}
+
+- (IBAction)spell7_action:(id)aSender
+{
+  [[RPGSFXEngine sharedSFXEngine] playSFXWithSpellID:7];
+}
 
 @end
