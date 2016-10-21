1ca0cb99184e4dfc47bb5ee48e7722763dc36984
Иван Дзюбенко <mimir@zeouniversity.com>
2016-10-21 15:34:43 +0300

    Add BattleManager class.

---
+++ RPG Game/RPGBattleManager.h
@@ -0,0 +1,13 @@
+//
+//  RPGBattleManager.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/21/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <SocketRocket/SocketRocket.h>
+
+@interface RPGBattleManager : SRWebSocket
+
+@end
---
+++ RPG Game/RPGBattleManager.m
@@ -0,0 +1,13 @@
+//
+//  RPGBattleManager.m
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/21/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGBattleManager.h"
+
+@implementation RPGBattleManager
+
+@end
