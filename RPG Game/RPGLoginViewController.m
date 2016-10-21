837d47ba05a5cc259e16811ad7bea3b553fa3d5e
Костянтин Паляничко <vercon@zeouniversity.com>
2016-10-21 17:52:12 +0300

    navigate through text fields on login screen

--- RPG Game/RPGLoginViewController.m
+++ RPG Game/RPGLoginViewController.m
@@ -148,4 +148,14 @@
   }
 }
 
+- (IBAction)userDoneEnteringUsername:(UITextField *)aSender
+{
+  [self.passwordInputField becomeFirstResponder];
+}
+
+- (IBAction)userDoneEnteringPassword:(UITextField *)aSender
+{
+  [self loginAction:self.loginButton];
+}
+
 @end
