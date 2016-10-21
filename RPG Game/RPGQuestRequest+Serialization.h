44a5fe145846f949d8040c6598f9ee7f677ac586
Максим Шульга <sunshine@zeouniversity.com>
2016-10-21 19:34:35 +0300

    Add RPGQuestRequest with serialization

---
+++ RPG Game/RPGQuestRequest+Serialization.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestRequest+Serialization.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/21/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGQuestRequest_Serialization : NSObject
+
+@end
---
+++ RPG Game/RPGQuestRequest+Serialization.m
@@ -0,0 +1,13 @@
+//
+//  RPGQuestRequest+Serialization.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/21/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestRequest+Serialization.h"
+
+@implementation RPGQuestRequest_Serialization
+
+@end
