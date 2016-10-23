4f55e75a91c013af8787b8e8749c8dda51d53b11
Иван Дзюбенко <mimir@zeouniversity.com>
2016-10-23 20:13:12 +0300

    Add serialization category.

---
+++ RPG Game/RPGBattleInitResponse+Serialization.m
@@ -0,0 +1,13 @@
+//
+//  RPGBattleInitResponse+Serialization.m
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/23/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGBattleInitResponse+Serialization.h"
+
+@implementation RPGBattleInitResponse (Serialization)
+
+@end
