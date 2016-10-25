2315ed5a3b50a36dc36dc3a52b63d8babdad00a7
Максим Шульга <sunshine@zeouniversity.com>
2016-10-25 15:47:26 +0300

    AddProof method in NetworkManager is done

--- RPG Game/RPGNetworkManager+Quests.m
+++ RPG Game/RPGNetworkManager+Quests.m
@@ -200,7 +200,100 @@
 
 - (void)addProofWithRequest:(RPGQuestRequest *)aRequest imageData:(NSData *)imageData completionHandler:(void (^)(NSInteger status))callbackBlock
 {
+  NSString *requestString = [NSString stringWithFormat:@"%@", @"http://10.55.33.28:8000/prove_quest"];
+  
+  NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:requestString]] autorelease];
+  
+  NSError *JSONSerializationError = nil;
+  request.HTTPMethod = @"POST";
+  
+  NSDictionary *requestDictionary = [aRequest dictionaryRepresentation];
+  NSData *requestJSONData = [NSJSONSerialization dataWithJSONObject:requestDictionary
+                                                        options:NSJSONWritingPrettyPrinted
+                                                          error:&JSONSerializationError];
+  
+  NSString *boundary = @"---------------------------Boundary Line---------------------------";
+  NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+  [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+  
+  NSMutableData *body = [[[NSMutableData alloc] init] autorelease];
+  
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:[@"Content-Disposition: form-data; name=\"json\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:requestJSONData];
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:[@"Content-Disposition: form-data; name=\"prove_image\"; filename=\"file.jpg\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+  [body appendData:imageData];
+  [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
 
+  request.HTTPBody = body;
+  
+  if (JSONSerializationError != nil)
+  {
+    [[NSException exceptionWithName:NSInvalidArgumentException
+                             reason:@"JSON cannot be retrieved from request"
+                           userInfo:nil] raise];
+  }
+  
+  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+  configuration.networkServiceType = NSURLNetworkServiceTypeDefault;
+  
+  NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+  
+  NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                          completionHandler:^(NSData * _Nullable data,
+                                                              NSURLResponse * _Nullable response,
+                                                              NSError * _Nullable error)
+  {
+    NSInteger status = 0;
+    NSError *JSONParsingError = nil;
+    RPGQuestResponse *responseObject = nil;
+    
+    if (error != nil)
+    {
+      status = 1;
+    }
+    
+    if (data != nil)
+    {
+      NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data
+                                                                         options:0
+                                                                           error:&JSONParsingError];
+      
+      if (JSONParsingError != nil)
+      {
+        // ???: tramper question
+        status = 3;
+      }
+      else
+      {
+        responseObject = [[[RPGQuestResponse alloc]
+                           initWithDictionaryRepresentation:responseDictionary] autorelease];
+      }
+      
+    }
+    else
+    {
+      status = 2;
+    }
+    
+    if (responseObject == nil)
+    {
+      status = 4;
+    }
+    
+    status = (status != 0) ? status : responseObject.status;
+    
+    dispatch_async(dispatch_get_main_queue(), ^
+    {
+      callbackBlock(status);
+    });
+  }];
+  
+  [task resume];
+  
+  [session finishTasksAndInvalidate];
 }
 
 @end
