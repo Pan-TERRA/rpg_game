276b9398456b99bfca5fdb56fb4255967e3746bf
Максим Шульга <sunshine@zeouniversity.com>
2016-10-26 16:44:50 +0300

    Add RPGQuestReviewRequest+Serialization class

---
+++ RPG Game/RPGQuestReviewRequest+Serialization.h
@@ -0,0 +1,16 @@
+//
+//  RPGQuestReviewRequest+Serialization.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/26/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestReviewRequest.h"
+
+@interface RPGQuestReviewRequest (Serialization)
+
+- (NSDictionary *)dictionaryRepresentation;
+- (instancetype)initWithDictionaryRepresentation:(NSDictionary *)aDictionary;
+
+@end
