3021bc97860277147699681f5a72d14e89f44420
Иван Дзюбенко <mimir@zeouniversity.com>
2016-10-26 16:31:22 +0300

    Add RPGSerializable protocol.

---
+++ RPG Game/RPGSerializable.h
@@ -0,0 +1,13 @@
+//
+//  RPGSerializable.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/26/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#ifndef RPGSerializable_h
+#define RPGSerializable_h
+
+
+#endif /* RPGSerializable_h */
