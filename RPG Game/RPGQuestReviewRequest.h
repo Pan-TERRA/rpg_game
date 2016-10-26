20541f7d6506a9c2b7ab6f4466e1e4d57c1d2746
Максим Шульга <sunshine@zeouniversity.com>
2016-10-26 16:44:29 +0300

    Add RPGQuestReviewRequest class

---
+++ RPG Game/RPGQuestReviewRequest.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestReviewRequest.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/26/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestRequest.h"
+
+@interface RPGQuestReviewRequest : RPGQuestRequest
+
+@end
---
+++ RPG Game/RPGQuestReviewRequest.m
@@ -0,0 +1,13 @@
+//
+//  RPGQuestReviewRequest.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/26/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestReviewRequest.h"
+
+@implementation RPGQuestReviewRequest
+
+@end
