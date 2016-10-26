504d366c9e39e0d74d3b617597d443274924d6f5
Максим Шульга <sunshine@zeouniversity.com>
2016-10-26 15:38:58 +0300

    Add RPGQuestAction.h

---
+++ RPG Game/RPGQuestAction.h
@@ -0,0 +1,13 @@
+//
+//  RPGQuestAction.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 10/26/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#ifndef RPGQuestAction_h
+#define RPGQuestAction_h
+
+
+#endif /* RPGQuestAction_h */
