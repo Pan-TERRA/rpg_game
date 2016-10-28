16c58083ab39b1b43d43b9a8aa5afd2b2743e00d
Максим Шульга <sunshine@zeouniversity.com>
2016-10-28 15:54:00 +0300

    Support all orientations

--- RPG Game/AppDelegate.m
+++ RPG Game/AppDelegate.m
@@ -79,14 +79,9 @@
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-//- (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
-//{
-//  return UIInterfaceOrientationMaskAll;
-//}
-
 - (UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
 {
-  return UIInterfaceOrientationMaskLandscape;
+  return UIInterfaceOrientationMaskAll;
 }
 
 @end
