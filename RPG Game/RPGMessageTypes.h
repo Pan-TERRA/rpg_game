b53615408a8c87f45372b36293cdd3991ae24639
Иван Дзюбенко <mimir@zeouniversity.com>
2016-10-31 14:40:40 +0200

    Add RPGMessageTypes

---
+++ RPG Game/RPGMessageTypes.h
@@ -0,0 +1,14 @@
+//
+//  RPGMessageTypes.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 10/31/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kRPGBattleInitMessageType;
+extern NSString * const kRPGBattleConditionMessageType;
+extern NSString * const kRPGSkillActionMessageType;
+extern NSString * const kRPGTimeRequestMessageType;
