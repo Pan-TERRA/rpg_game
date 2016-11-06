8016203ac30e330a452472fc60fff970866ef931
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-06 16:15:20 +0200

    Style fix, group properties.

--- RPG Game/RPGQuestViewController.m
+++ RPG Game/RPGQuestViewController.m
@@ -6,37 +6,45 @@
 //  Copyright © 2016 RPG-team. All rights reserved.
 //
 
+#import <AVFoundation/AVFoundation.h>
 #import "RPGQuestViewController.h"
+  // API
+#import "RPGNetworkManager+Quests.h"
+  // Views
 #import "RPGQuestListViewController.h"
 #import "RPGQuestProofImageViewController.h"
-#import "RPGNibNames.h"
+  // Entities
 #import "RPGQuest+Serialization.h"
 #import "RPGQuestReward+Serialization.h"
-#import "RPGNetworkManager+Quests.h"
 #import "RPGQuestRequest+Serialization.h"
 #import "RPGQuestReviewRequest+Serialization.h"
+  // Misc
 #import "NSUserDefaults+RPGSessionInfo.h"
-#import "RPGQuestAction.h"
-#import <AVFoundation/AVFoundation.h>
-#import "RPGStatusCodes.h"
 #import "RPGAlert.h"
+  // Constants
+#import "RPGNibNames.h"
+#import "RPGQuestAction.h"
 
 @interface RPGQuestViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
-@property (nonatomic, assign, readwrite) IBOutlet UIButton *acceptButton;
-@property (nonatomic, assign, readwrite) IBOutlet UIButton *denyButton;
-@property (nonatomic, assign, readwrite) IBOutlet UIButton *addProofButton;
 @property (nonatomic, assign, readwrite) IBOutlet UILabel *proofLabel;
 @property (nonatomic, assign, readwrite) IBOutlet UIImageView *proofImageView;
-@property (nonatomic, assign, readwrite) IBOutlet UILabel *titleLabel;
-@property (nonatomic, assign, readwrite) IBOutlet UILabel *descriptionLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *stateTitleLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *stateLabel;
 @property (nonatomic, assign, readwrite) IBOutlet UILabel *crystalsRewardLabel;
 @property (nonatomic, assign, readwrite) IBOutlet UILabel *goldRewardLabel;
 @property (nonatomic, assign, readwrite) IBOutlet UIImageView *skillRewardImageView;
-@property (nonatomic, assign, readwrite) IBOutlet UILabel *stateTitleLabel;
-@property (nonatomic, assign, readwrite) IBOutlet UILabel *stateLabel;
+
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *titleLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *descriptionLabel;
+
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *addProofButton;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *acceptButton;
+@property (nonatomic, assign, readwrite) IBOutlet UIButton *denyButton;
+
 @property (nonatomic, assign, readwrite) RPGQuestState state;
 @property (nonatomic, assign, readwrite) NSUInteger questID;
+
 @property (nonatomic, copy, readwrite) NSString *proofImageStringURL;
 @property (nonatomic, retain, readwrite) UIImagePickerController *imagePickerController;
 
@@ -91,6 +99,7 @@
 - (void)viewDidAppear:(BOOL)animated
 {
   [super viewDidAppear:animated];
+  
   switch (self.state)
   {
     case kRPGQuestStateDone:
