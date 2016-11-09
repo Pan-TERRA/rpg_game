6896cd0cad13de8b33db18bffb11479edbcaa650
Максим Шульга <sunshine@zeouniversity.com>
2016-11-09 17:35:50 +0200

    RPGBattleConditionResponse+Serialization with RPGResources for reward

--- RPG Game/RPGBattleConditionResponse+Serialization.m
+++ RPG Game/RPGBattleConditionResponse+Serialization.m
@@ -8,6 +8,7 @@
 
 #import "RPGBattleConditionResponse+Serialization.h"
 #import "RPGResponse+Serialization.h"
+#import "RPGResources+Serialization.h"
 
 NSString * const kRPGBattleConditionResponseHP = @"hp";
 NSString * const kRPGBattleConditionResponseOpponentHP = @"opponent_hp";
@@ -27,7 +28,7 @@ NSString * const kRPGBattleConditionResponseCurrentTurn = @"is_current_turn";
   dictionaryRepresentation[kRPGBattleConditionResponseOpponentHP] = @(self.opponentHP);
   dictionaryRepresentation[kRPGBattleConditionResponseSkillsCondition] = self.skillsCondition;
   dictionaryRepresentation[kRPGBattleConditionResponseSkillsDamage] = self.skillsDamage;
-  dictionaryRepresentation[kRPGBattleConditionResponseReward] = self.reward;
+  dictionaryRepresentation[kRPGBattleConditionResponseReward] = [self.reward dictionaryRepresentation];
   dictionaryRepresentation[kRPGBattleConditionResponseStatus] = @(self.status);
   dictionaryRepresentation[kRPGBattleConditionResponseCurrentTurn] = @(self.currentTurn);
   
@@ -36,11 +37,12 @@ NSString * const kRPGBattleConditionResponseCurrentTurn = @"is_current_turn";
 
 - (instancetype)initWithDictionaryRepresentation:(NSDictionary *)aDictionary
 {
+  RPGResources *resources = [[[RPGResources alloc] initWithDictionaryRepresentation:aDictionary[kRPGBattleConditionResponseReward]] autorelease];
   return [self initWithHP:[aDictionary[kRPGBattleConditionResponseHP] integerValue]
                opponentHP:[aDictionary[kRPGBattleConditionResponseOpponentHP] integerValue]
           skillsCondition:aDictionary[kRPGBattleConditionResponseSkillsCondition]
              skillsDamage:aDictionary[kRPGBattleConditionResponseSkillsDamage]
-                   reward:aDictionary[kRPGBattleConditionResponseReward]
+                   reward:resources
                    status:[aDictionary[kRPGBattleConditionResponseStatus] integerValue]
               currentTurn:[aDictionary[kRPGBattleConditionResponseCurrentTurn] boolValue]];
 }
