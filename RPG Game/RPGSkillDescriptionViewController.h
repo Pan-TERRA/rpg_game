c15eac2e9688668338d9bc8cf351aceed0402678
Владислав Крут <pantera@zeouniversity.com>
2016-11-09 16:25:51 +0200

    Create RPGSkillDescriptionViewController; add skill icons to assets, rename six in bundle

---
+++ RPG Game/RPGSkillDescriptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPGSkillDescriptionViewController.h
+//  RPG Game
+//
+//  Created by Владислав Крут on 11/8/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPGSkillDescriptionViewController : UIViewController
+
+@end
---
+++ RPG Game/RPGSkillDescriptionViewController.m
@@ -0,0 +1,27 @@
+//
+//  RPGSkillDescriptionViewController.m
+//  RPG Game
+//
+//  Created by Владислав Крут on 11/8/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGSkillDescriptionViewController.h"
+
+@interface RPGSkillDescriptionViewController ()
+
+@end
+
+@implementation RPGSkillDescriptionViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
