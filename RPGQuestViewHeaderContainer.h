5f3a4b36f28366763f67a3e18b6fe4f51817af9c
Максим Шульга <sunshine@zeouniversity.com>
2016-11-10 20:10:50 +0200

    Add HeaderContainer view

---
+++ RPGQuestViewHeaderContainer.h
@@ -0,0 +1,21 @@
+//
+//  RPGQuestViewHeaderContainer.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 11/10/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RPGQuestViewController;
+@class RPGQuestReward;
+
+@interface RPGQuestViewHeaderContainer : UIView
+
+@property (nonatomic, assign, readwrite) RPGQuestViewController *questViewController;
+
+- (void)setViewContent:(RPGQuestReward *)aQuestReward;
+- (void)updateView;
+
+@end
---
+++ RPGQuestViewHeaderContainer.m
@@ -0,0 +1,136 @@
+//
+//  RPGQuestViewHeaderContainer.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 11/10/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGQuestViewHeaderContainer.h"
+
+#import "RPGQuestReward.h"
+#import "RPGQuest.h"
+#import "RPGQuestViewController.h"
+
+@interface RPGQuestViewHeaderContainer()
+
+@property (nonatomic, assign, readwrite) IBOutlet UIImageView *proofTypeImageView;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *stateTitleLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UIImageView *stateImageView;
+
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *crystalsRewardLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UILabel *goldRewardLabel;
+@property (nonatomic, assign, readwrite) IBOutlet UIImageView *skillRewardImageView;
+
+@end
+
+@implementation RPGQuestViewHeaderContainer
+
+#pragma mark - View Content
+
+- (void)setViewContent:(RPGQuestReward *)aQuestReward
+{
+  self.crystalsRewardLabel.text = [@(aQuestReward.crystals) stringValue];
+  self.goldRewardLabel.text = [@(aQuestReward.gold) stringValue];
+  
+  if (aQuestReward.skillID != 0)
+  {
+    
+  }
+  else
+  {
+    UIImageView *skillRewardView = self.skillRewardImageView;
+    UILabel *goldRewardLabel = self.goldRewardLabel;
+    [skillRewardView removeConstraint:[NSLayoutConstraint constraintWithItem:skillRewardView
+                                                                   attribute:NSLayoutAttributeTrailing
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:skillRewardView.superview
+                                                                   attribute:NSLayoutAttributeTrailing
+                                                                  multiplier:1.0
+                                                                    constant:10]];
+    [skillRewardView removeConstraint:[NSLayoutConstraint constraintWithItem:skillRewardView
+                                                                   attribute:NSLayoutAttributeLeading
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:goldRewardLabel
+                                                                   attribute:NSLayoutAttributeTrailing
+                                                                  multiplier:1.0
+                                                                    constant:10]];
+    [skillRewardView removeFromSuperview];
+    [goldRewardLabel.superview addConstraint:[NSLayoutConstraint constraintWithItem:goldRewardLabel.superview
+                                                                          attribute:NSLayoutAttributeTrailing
+                                                                          relatedBy:NSLayoutRelationEqual
+                                                                             toItem:goldRewardLabel
+                                                                          attribute:NSLayoutAttributeTrailing
+                                                                         multiplier:1.0
+                                                                           constant:10]];
+  }
+  
+  
+  
+}
+
+#pragma mark - View State
+
+- (void)updateView
+{
+  switch (self.questViewController.state)
+  {
+    case kRPGQuestStateCanTake:
+    case kRPGQuestStateForReview:
+    case kRPGQuestStateReviewedTrue:
+    {
+      [self setStateItemsHidden:YES];
+      break;
+    }
+    case kRPGQuestStateInProgress:
+    case kRPGQuestStateDone:
+    case kRPGQuestStateReviewedFalse:
+    {
+      [self setStateItemsHidden:NO];
+      break;
+    }
+  }
+}
+
+- (void)setStateItemsHidden:(BOOL)aFlag
+{
+  if (aFlag)
+  {
+    UIImageView *stateImageView = self.stateImageView;
+    UIImageView *proofTypeImageView = self.proofTypeImageView;
+    UILabel *stateTitleLabel = self.stateTitleLabel;
+    [stateImageView removeConstraint:[NSLayoutConstraint constraintWithItem:stateImageView
+                                                                  attribute:NSLayoutAttributeTrailing
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:stateImageView.superview
+                                                                  attribute:NSLayoutAttributeTrailing
+                                                                 multiplier:1.0
+                                                                   constant:10]];
+    [stateImageView removeConstraint:[NSLayoutConstraint constraintWithItem:stateImageView
+                                                                  attribute:NSLayoutAttributeLeading
+                                                                  relatedBy:NSLayoutRelationEqual
+                                                                     toItem:stateTitleLabel
+                                                                  attribute:NSLayoutAttributeTrailing
+                                                                 multiplier:1.0
+                                                                   constant:10]];
+    [stateImageView removeFromSuperview];
+    [stateTitleLabel removeConstraint:[NSLayoutConstraint constraintWithItem:stateTitleLabel
+                                                                   attribute:NSLayoutAttributeLeading
+                                                                   relatedBy:NSLayoutRelationEqual
+                                                                      toItem:proofTypeImageView
+                                                                   attribute:NSLayoutAttributeTrailing
+                                                                  multiplier:1.0
+                                                                    constant:10]];
+    
+    [stateTitleLabel removeFromSuperview];
+    [proofTypeImageView.superview addConstraint:[NSLayoutConstraint constraintWithItem:proofTypeImageView.superview
+                                                                             attribute:NSLayoutAttributeTrailing
+                                                                             relatedBy:NSLayoutRelationEqual
+                                                                                toItem:proofTypeImageView
+                                                                             attribute:NSLayoutAttributeTrailing
+                                                                            multiplier:1.0
+                                                                              constant:10]];
+  }
+}
+
+@end
