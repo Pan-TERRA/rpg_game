85879420114225909650ffdf5ea31c27268a4fe1
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-15 13:25:08 +0200

    Add UIWindow support category.

---
+++ RPG Game/UIWindow+RPGPresentController.h
@@ -0,0 +1,15 @@
+//
+//  UIWindow+RPGPresentController.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 11/15/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (RPGPresentController)
+
+- (UIViewController *) visibleViewController;
+
+@end
