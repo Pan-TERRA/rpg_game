172e48f2de5ddc50f068ded2b1832a1746b95a3f
Максим Шульга <sunshine@zeouniversity.com>
2016-11-17 11:35:44 +0200

    Create NetworkManager+CharacterProfile class

---
+++ RPG Game/RPGNetworkManager+CharacterProfile.h
@@ -0,0 +1,13 @@
+//
+//  RPGNetworkManager+CharacterProfile.h
+//  RPG Game
+//
+//  Created by Максим Шульга on 11/17/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGNetworkManager.h"
+
+@interface RPGNetworkManager (CharacterProfile)
+
+@end
---
+++ RPG Game/RPGNetworkManager+CharacterProfile.m
@@ -0,0 +1,13 @@
+//
+//  RPGNetworkManager+CharacterProfile.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 11/17/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGNetworkManager+CharacterProfile.h"
+
+@implementation RPGNetworkManager (CharacterProfile)
+
+@end
