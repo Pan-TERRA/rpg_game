fbab9f0981623eace20f81895ee9fe18869ac442
Максим Шульга <sunshine@zeouniversity.com>
2016-11-17 13:06:52 +0200

    Add RPGSkill entity

---
+++ RPG Game/RPGSkill.m
@@ -0,0 +1,66 @@
+//
+//  RPGSkill.m
+//  RPG Game
+//
+//  Created by Максим Шульга on 11/17/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGSkill.h"
+
+NSString * const kRPGSkillID = @"skill_id";
+NSString * const kRPGSkillSelected = @"is_selected";
+
+@interface RPGSkill()
+
+@property (nonatomic, assign, readwrite) NSUInteger skillID;
+@property (nonatomic, assign, readwrite) BOOL selected;
+
+@end
+
+@implementation RPGSkill
+
+- (instancetype)initWithID:(NSUInteger)aSkillID
+                  selected:(BOOL)isSelected
+{
+  self = [super init];
+  
+  if (self != nil)
+  {
+    _skillID = aSkillID;
+    _selected = isSelected;
+  }
+  
+  return self;
+}
+
++ (instancetype)skillWithID:(NSUInteger)aSkillID
+                   selected:(BOOL)isSelected
+{
+  return [[[self alloc] initWithID:aSkillID
+                          selected:isSelected] autorelease];
+}
+
+- (instancetype)init
+{
+  return [self initWithID:0
+                 selected:NO];
+}
+
+#pragma mark - RPGSerializable
+
+- (NSDictionary *)dictionaryRepresentation
+{
+  NSMutableDictionary *dictionaryRepresentation = [NSMutableDictionary dictionary];
+  dictionaryRepresentation[kRPGSkillID] = @(self.skillID);
+  dictionaryRepresentation[kRPGSkillSelected] = @(self.isSelected);
+  return dictionaryRepresentation;
+}
+
+- (instancetype)initWithDictionaryRepresentation:(NSDictionary *)aDictionary
+{
+  return [self initWithID:[aDictionary[kRPGSkillID] integerValue]
+                 selected:[aDictionary[kRPGSkillID] boolValue]];
+}
+
+@end
