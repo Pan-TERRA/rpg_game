b9bbc4727ace86b919010c0c958de594cebe353c
Костянтин Паляничко <vercon@zeouniversity.com>
2016-11-20 23:41:45 +0200

    add RPGArenaBag entity

---
+++ RPG Game/RPGArenaBag.h
@@ -0,0 +1,13 @@
+//
+//  RPGArenaBag.h
+//  RPG Game
+//
+//  Created by Костянтин Паляничко on 11/20/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RPGArenaBag : NSObject
+
+@end
---
+++ RPG Game/RPGArenaBag.m
@@ -0,0 +1,13 @@
+//
+//  RPGArenaBag.m
+//  RPG Game
+//
+//  Created by Костянтин Паляничко on 11/20/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGArenaBag.h"
+
+@implementation RPGArenaBag
+
+@end
