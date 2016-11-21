73556dffb073a5eb02fb497014daf25e69243b3d
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-21 14:30:04 +0200

    Update error handling category.

--- RPG Game/RPGAlertController+RPGErrorHandling.h
+++ RPG Game/RPGAlertController+RPGErrorHandling.h
@@ -7,58 +7,12 @@
 //
 
 #import "RPGAlertController.h"
+  // Constants
+#import "RPGStatusCodes.h"
 
 @interface RPGAlertController (RPGErrorHandling)
 
-#pragma mark - General Client-Server Errors
-
-+ (void)handleWrongJSON;
-+ (void)handleUserDoesNotExist;
-+ (void)handleWrongEmail;
-+ (void)handleWrongPassword;
-+ (void)handleWrongToken;
-+ (void)handleUsernameAlreadyTaken;
-+ (void)handleEmailAlreadyTaken;
-
-#pragma mark Quests
-
-+ (void)handleQuestWithSuchIDNotFound;
-+ (void)handleQuestAlreadyInProgress;
-+ (void)handleServerDidNotGetFile;
-+ (void)handleUserHasNoSuchQuest;
-+ (void)handleNotPicture;
-+ (void)handleTooBigPicture;
-+ (void)handleTurnAction;
-+ (void)handleQuestNotAccepted;
-+ (void)handleQuestNotSkipped;
-+ (void)handleNoQuestForVoting;
-
-#pragma mark Character
-
-+ (void)handleUserHasNoSuchCharacter;
-
-#pragma mark Skills
-
-+ (void)handleSkillWithSuchIDNotFound;
-
-#pragma mark Battle
-
-+ (void)handleNotYourTurn;
-+ (void)handleSkillOnCooldownOrDoesNotExist;
-+ (void)handleRunOutOfTime;
-+ (void)handleBattleDoesNotExist;
-
-#pragma mark Class
-
-+ (void)handleWrongClass;
-+ (void)handleClassNotFound;
-+ (void)handleClassesNotFound;
-
-#pragma mark - Network Manager Errors
-
-+ (void)handleNoInternetConnection;
-
-#pragma mark - Default
++ (void)showErrorWithStatusCode:(RPGStatusCode)aStatusCode;
 
 + (void)handleDefaultError;
 
