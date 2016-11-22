1aebc92b9975c5f6cbbdd6fc61869664e1eca54d
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-22 16:41:35 +0200

    Update battle init response with player entity.

--- RPG Game/RPGBattleInitResponse.h
+++ RPG Game/RPGBattleInitResponse.h
@@ -1,17 +1,19 @@
-//
-//  RPGBattleInitResponse.h
-//  RPG Game
-//
-//  Created by Иван Дзюбенко on 10/11/16.
-//  Copyright © 2016 RPG-team. All rights reserved.
-//
+  //
+  //  RPGBattleInitResponse.h
+  //  RPG Game
+  //
+  //  Created by Иван Дзюбенко on 10/11/16.
+  //  Copyright © 2016 RPG-team. All rights reserved.
+  //
 
 #import "RPGResponse.h"
 
 @class RPGEntity;
+@class RPGPlayer;
 
 @interface RPGBattleInitResponse : RPGResponse
 
+@property (nonatomic, assign, readonly) RPGPlayer *playerInfo;
 @property (nonatomic, retain, readonly) RPGEntity *opponentInfo;
 @property (nonatomic, assign, readonly, getter=isCurrentTurn) BOOL currentTurn;
 @property (nonatomic, assign, readonly) NSInteger time;
@@ -20,10 +22,13 @@
 #pragma mark - Init
 
 - (instancetype)initWithOpponentInfo:(RPGEntity *)anOpponentInfo
+                          playerInfo:(RPGPlayer *)aPlayerInfo
                          currentTurn:(BOOL)aCurrentTurn
                                 time:(NSInteger)aTime
                               status:(NSInteger)aStatus NS_DESIGNATED_INITIALIZER;
+
 + (instancetype)battleInitResponseWithOpponentInfo:(RPGEntity *)anOpponentInfo
+                                        playerInfo:(RPGPlayer *)aPlayerInfo
                                        currentTurn:(BOOL)aCurrentTurn
                                               time:(NSInteger)aTime
                                             status:(NSInteger)aStatus;
