37066f3e00c3e6811bef2ca63223088b137fc2fb
Максим Шульга <sunshine@zeouniversity.com>
2016-11-23 14:08:04 +0200

    Fix RPGAlertController bug

--- RPG Game/RPGAlertController.m
+++ RPG Game/RPGAlertController.m
@@ -30,12 +30,12 @@ static NSString * const kRPGAlertControllerDefaultActionTitle = @"Ok";
   
   if (aMessage == nil)
   {
-    aTitle = kRPGAlertControllerDefaultMessage;
+    aMessage = kRPGAlertControllerDefaultMessage;
   }
   
   if (anActionTitle == nil)
   {
-    aTitle = kRPGAlertControllerDefaultActionTitle;
+    anActionTitle = kRPGAlertControllerDefaultActionTitle;
   }
   
   RPGAlertViewController *alertViewController = [[RPGAlertViewController alloc] initWithTitle:aTitle
