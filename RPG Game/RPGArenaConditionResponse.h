5e9785b6484968e2ed468db45fdd14bcab6d9dee
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-27 15:41:42 +0200

    Add future arena condition response object.

---
+++ RPG Game/RPGArenaConditionResponse.h
@@ -0,0 +1,13 @@
+//
+//  RPGArenaConditionResponse.h
+//  RPG Game
+//
+//  Created by Иван Дзюбенко on 11/27/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGBattleConditionResponse.h"
+
+@interface RPGArenaConditionResponse : RPGBattleConditionResponse
+
+@end
