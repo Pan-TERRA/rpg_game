0e4abddbe01effc86789c63130d58d1adeedbeb4
Иван Дзюбенко <mimir@zeouniversity.com>
2016-11-28 17:49:12 +0200

    Update websocket message types.

--- RPG Game/RPGMessageTypes.m
+++ RPG Game/RPGMessageTypes.m
@@ -9,11 +9,11 @@
 #import "RPGMessageTypes.h"
 
   // Arena
-NSString * const kRPGArenaInitMessageType = @"ARENA_INIT";
-NSString * const kRPGArenaConditionMessageType = @"ARENA_CONDITION";
+NSString * const kRPGArenaInitMessageType = @"ARENA_BATTLE_INIT";
+NSString * const kRPGArenaConditionMessageType = @"ARENA_BATTLE_CONDITION";
   // Adventures
-NSString * const kRPGAdventuresInitMessageType = @"BATTLE_INIT";
-NSString * const kRPGAdventuresConditionMessageType = @"BATTLE_CONDITION";
+NSString * const kRPGAdventuresInitMessageType = @"ADVENTURE_BATTLE_INIT";
+NSString * const kRPGAdventuresConditionMessageType = @"ADVENTURE_BATTLE_CONDITION";
   // General
 NSString * const kRPGSkillActionMessageType = @"SKILL_ACTION";
 NSString * const kRPGTimeRequestMessageType = @"TIME_REQUEST";
