bfd60f7c879828705d23e5b690520646b537bfee
Максим Шульга <sunshine@zeouniversity.com>
2016-11-30 18:13:47 +0200

    Fix RPGAuthorizationLoginResponse

--- RPG Game/RPGAuthorizationLoginResponse.h
+++ RPG Game/RPGAuthorizationLoginResponse.h
@@ -14,26 +14,17 @@
 @property (nonatomic, assign, readonly) NSInteger status;
 @property (nonatomic, copy, readonly) NSString *username;
 @property (nonatomic, copy, readonly) NSString *token;
-@property (nonatomic, copy, readonly) NSString *avatar;
-@property (nonatomic, assign, readonly) NSInteger gold;
-@property (nonatomic, assign, readonly) NSInteger crystals;
 @property (nonatomic, retain, readonly) NSDictionary *character;
 
 #pragma mark - Init
 
 - (instancetype)initWithUsername:(NSString *)aUsername
                            token:(NSString *)aToken
-                          avatar:(NSString *)anAvatar
-                            gold:(NSInteger)aGold
-                        crystals:(NSInteger)aCrystals
-                      character:(NSDictionary *)aCharacter
+                       character:(NSDictionary *)aCharacter
                           status:(NSInteger)aStatus NS_DESIGNATED_INITIALIZER;
 + (instancetype)responseWithUsername:(NSString *)aUsername
                                token:(NSString *)aToken
-                              avatar:(NSString *)anAvatar
-                                gold:(NSInteger)aGold
-                            crystals:(NSInteger)aCrystals
-                          character:(NSDictionary *)aCharacter
+                           character:(NSDictionary *)aCharacter
                               status:(NSInteger)aStatus;
 
 #pragma mark - Actions
