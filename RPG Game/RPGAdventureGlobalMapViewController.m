66ec97b88014cf875022ce95dccb56ed925593be
Степан Супинский <ssup@zeouniversity.com>
2016-12-03 18:19:45 +0200

    Determine tapped map location id Add pragma statements, comments

--- RPG Game/RPGAdventureGlobalMapViewController.m
+++ RPG Game/RPGAdventureGlobalMapViewController.m
@@ -7,14 +7,26 @@
 //
 
 #import "RPGAdventureGlobalMapViewController.h"
-#import "RPGNibNames.h"
+  // Misc
 #import "UIView+RPGColorOfPoint.h"
+#import "UIColor+RPGColorEquality.h"
+  // Constants
+#import "RPGNibNames.h"
+
+  // Global map location mask colors
+#define GLOBAL_MAP_LOCATION1_MASK_COLOR [UIColor colorWithRed:0.32549 green:0.219608 blue:0.113725 alpha:1.0]
+#define GLOBAL_MAP_LOCATION2_MASK_COLOR [UIColor colorWithRed:0.647059 green:0.411765 blue:0.411765 alpha:1.0]
+#define GLOBAL_MAP_LOCATION3_MASK_COLOR [UIColor colorWithRed:0.266667 green:0.945098 blue:0.00392157 alpha:1.0]
+#define GLOBAL_MAP_LOCATION4_MASK_COLOR [UIColor colorWithRed:0.27451 green:0.235294 blue:0.752941 alpha:1.0]
 
 @interface RPGAdventureGlobalMapViewController ()
 
+#pragma - Outlets
+
+  // View outlets
 @property (readwrite, assign, nonatomic) IBOutlet UIImageView *maskImageView;
 @property (readwrite, assign, nonatomic) IBOutlet UIImageView *displayImageView;
-
+  // Constraint outlets
 @property (readwrite, assign, nonatomic) IBOutlet NSLayoutConstraint *contentViewWidthConstraint;
 @property (readwrite, assign, nonatomic) IBOutlet NSLayoutConstraint *contentViewHeightConstraint;
 
@@ -22,10 +34,14 @@
 
 @implementation RPGAdventureGlobalMapViewController
 
+#pragma mark Init
+
 - (instancetype)init
 {
   return [self initWithNibName:kRPGAdventureGlobalMapViewControllerNIBName bundle:nil];
 }
+
+#pragma mark - UIViewController
 
 - (void)viewDidLayoutSubviews
 {
@@ -33,14 +49,35 @@
   self.contentViewHeightConstraint.constant = self.maskImageView.frame.size.height;
 }
 
+#pragma mark - Map
+
 - (IBAction)mapTappedAction:(UITapGestureRecognizer *)gestureRecognizer
 {
   CGPoint tapLocation = [gestureRecognizer locationInView:gestureRecognizer.view];
-  NSLog(@"%@", [self.maskImageView colorOfPoint:tapLocation]);
+  UIColor *tappedLocationColor = [self.maskImageView colorOfPoint:tapLocation];
+  NSInteger tappedLocationID = [self getMapLocationIDWithColor:tappedLocationColor];
+
+  NSLog(@"%ld", tappedLocationID);
 }
 
-- (void)dealloc
+- (NSInteger)getMapLocationIDWithColor:(UIColor *)aColor
 {
-  [super dealloc];
+  NSInteger result = -1;
+  NSArray<UIColor *> *mapLocationColors = @[
+                                 GLOBAL_MAP_LOCATION1_MASK_COLOR,
+                                 GLOBAL_MAP_LOCATION2_MASK_COLOR,
+                                 GLOBAL_MAP_LOCATION3_MASK_COLOR,
+                                 GLOBAL_MAP_LOCATION4_MASK_COLOR
+                                 ];
+  for (NSInteger i = 0; i < mapLocationColors.count; i++)
+  {
+    if ([aColor isEqualToColor:mapLocationColors[i]])
+    {
+      result = i + 1;
+    }
+  }
+  
+  return result;
 }
+
 @end
