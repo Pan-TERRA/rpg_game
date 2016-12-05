97d0933ea7382cd5f1a4d91d0c6d7c2c452f8a4a
Максим Шульга <sunshine@zeouniversity.com>
2016-12-05 19:25:55 +0200

    Fix Battle responses with RPGPlayerInfo entity

--- RPG Game/RPGAdventuresConditionResponse.h
+++ RPG Game/RPGAdventuresConditionResponse.h
@@ -8,24 +8,26 @@
 
 #import "RPGBattleConditionResponse.h"
 
+@class RPGPlayerInfo;
+
 @interface RPGAdventuresConditionResponse : RPGBattleConditionResponse
 
-- (instancetype)initWithHP:(NSInteger)aHP
-                  opponentHP:(NSInteger)anOpponentHP
-             skillsCondition:(NSArray *)aSkillsCondition
-                skillsDamage:(NSDictionary *)aSkillsDamage
-                battleStatus:(RPGBattleStatus)aBattleStatus
-                      reward:(RPGBattleReward *)aReward
-                      status:(NSInteger)aStatus
-                 currentTurn:(BOOL)aCurrentTurn NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPlayerInfo:(RPGPlayerInfo *)aPlayerInfo
+                      opponentInfo:(RPGPlayerInfo *)anOpponentInfo
+                   skillsCondition:(NSArray *)aSkillsCondition
+                      skillsDamage:(NSDictionary *)aSkillsDamage
+                      battleStatus:(RPGBattleStatus)aBattleStatus
+                            reward:(RPGBattleReward *)aReward
+                            status:(NSInteger)aStatus
+                       currentTurn:(BOOL)aCurrentTurn NS_DESIGNATED_INITIALIZER;
 
-+ (instancetype)battleConditionResponseWithHP:(NSInteger)aHP
-                                     opponentHP:(NSInteger)anOpponentHP
-                                skillsCondition:(NSArray *)aSkillsCondition
-                                   skillsDamage:(NSDictionary *)aSkillsDamage
-                                   battleStatus:(RPGBattleStatus)aBattleStatus
-                                         reward:(RPGBattleReward *)aReward
-                                         status:(NSInteger)aStatus
-                                    currentTurn:(BOOL)aCurrentTurn;
++ (instancetype)battleConditionResponseWithPlayerInfo:(RPGPlayerInfo *)aPlayerInfo
+                                         opponentInfo:(RPGPlayerInfo *)anOpponentInfo
+                                      skillsCondition:(NSArray *)aSkillsCondition
+                                         skillsDamage:(NSDictionary *)aSkillsDamage
+                                         battleStatus:(RPGBattleStatus)aBattleStatus
+                                               reward:(RPGBattleReward *)aReward
+                                               status:(NSInteger)aStatus
+                                          currentTurn:(BOOL)aCurrentTurn;
 
 @end
