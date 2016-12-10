2d07ee7fb9991984443bf50451836aedf5bb6c86
Костянтин Паляничко <vercon@zeouniversity.com>
2016-12-10 20:24:51 +0200

    fix arena controller

--- RPG Game/RPGArenaController.m
+++ RPG Game/RPGArenaController.m
@@ -11,6 +11,10 @@
 #import "RPGWebsocketManager.h"
   // Entities
 #import "RPGArenaInitRequest.h"
+#import "RPGSkill.h"
+#import "RPGBattle.h"
+#import "RPGPlayer.h"
+#import "RPGAdventuresInitResponse.h"
   // Misc
 #import "RPGSerializable.h"
 #import "NSUserDefaults+RPGSessionInfo.h"
@@ -35,7 +39,36 @@
 
 #pragma mark Process Manager Response
 
-#pragma mark Battle Init Response
+#pragma mark - Battle Init Response
+
+- (void)processBattleInitResponse:(NSDictionary *)aResponse
+{
+  RPGAdventuresInitResponse *battleInitResponse = [[[RPGAdventuresInitResponse alloc]
+                                                    initWithDictionaryRepresentation:aResponse]
+                                                   autorelease];
+  
+  if (battleInitResponse != nil && battleInitResponse.status == 0)
+  {
+    self.battle = [RPGBattle battleWithBattleInitResponse:battleInitResponse];
+    
+    NSUserDefaults *standardUserDefaults = [NSUserDefaults standardUserDefaults];
+    NSArray *skillIDs = standardUserDefaults.selectedArenaSkills;
+    
+    NSMutableArray *skills = [NSMutableArray array];
+    for (NSNumber *skillID in skillIDs)
+    {
+      RPGSkill *skill = [[RPGSkill alloc] initWithSkillID:[skillID integerValue]];
+      [skills addObject:skill];
+      [skill release];
+    }
+    self.battle.player.skills = skills;
+    
+    [[NSNotificationCenter defaultCenter] postNotificationName:kRPGBattleInitDidEndSetUpNotification
+                                                        object:self];
+    [[NSNotificationCenter defaultCenter] postNotificationName:kRPGModelDidChangeNotification
+                                                        object:self];
+  }
+}
 
 #pragma mark Battle Condition Response
 
