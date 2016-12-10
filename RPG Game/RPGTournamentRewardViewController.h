35ea1d3df40384d84ae01a404b539afd99024419
Костянтин Паляничко <vercon@zeouniversity.com>
2016-12-10 20:34:20 +0200

    subclass tournamentRewardViewController

---
+++ RPG Game/RPGTournamentRewardViewController.h
@@ -0,0 +1,13 @@
+//
+//  RPGTournamentRewardViewController.h
+//  RPG Game
+//
+//  Created by Костянтин Паляничко on 12/8/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGRewardViewController.h"
+
+@interface RPGTournamentRewardViewController : RPGRewardViewController
+
+@end
---
+++ RPG Game/RPGTournamentRewardViewController.m
@@ -0,0 +1,56 @@
+//
+//  RPGTournamentRewardViewController.m
+//  RPG Game
+//
+//  Created by Костянтин Паляничко on 12/8/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import "RPGTournamentRewardViewController.h"
+  // Controllers
+#import "RPGTournamentController.h"
+#import "RPGTournamentRewardRankViewController.h"
+  // Misc
+#import "UIViewController+RPGChildViewController.h"
+
+@interface RPGTournamentRewardViewController ()
+
+@property (nonatomic, retain, readwrite) RPGTournamentRewardRankViewController *rankViewController;
+
+@end
+
+@implementation RPGTournamentRewardViewController
+
+#pragma mark - Init
+
+- (instancetype)initWithBattleController:(RPGBattleController *)aBattleController
+{
+  self = [super initWithBattleController:aBattleController];
+  
+  if (self != nil)
+  {
+    _rankViewController = [[RPGTournamentRewardRankViewController alloc] initWithBattleController:aBattleController];
+  }
+  
+  return self;
+}
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  
+  [self addChildViewController:self.rankViewController view:self.rankContainer];
+}
+
+#pragma mark - RPGRewardViewController
+
+- (void)updateView
+{
+  [super updateView];
+  
+  [self.rankViewController updateView];
+}
+
+@end
