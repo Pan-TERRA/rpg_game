e2e9ac7a9c98089a378f2f710ad18a9f8453b5f5
Костянтин Паляничко <vercon@zeouniversity.com>
2016-12-12 20:10:58 +0200

    create protocol instead of abstract factory

--- RPG Game/RPGBattleFactoryProtocol.h
+++ RPG Game/RPGBattleFactoryProtocol.h
@@ -1,17 +1,20 @@
 //
-//  RPGBattleFactory.h
+//  RPGBattleFactoryProtocol.h
 //  RPG Game
 //
-//  Created by Костянтин Паляничко on 12/8/16.
+//  Created by Костянтин Паляничко on 12/12/16.
 //  Copyright © 2016 RPG-team. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-  // Controlelers
-@class RPGRewardViewController;
-@class RPGBattleController;
 
-@interface RPGBattleFactory : NSObject
+@class RPGBattleController;
+@class RPGRewardViewController;
+
+/**
+ *  Abstract factory. Used by battleViewController and its subclasses
+ */
+@protocol RPGBattleFactoryProtocol <NSObject>
 
 @property (retain, nonatomic, readonly) RPGBattleController *battleController;
 @property (retain, nonatomic, readonly) RPGRewardViewController *rewardViewController;
