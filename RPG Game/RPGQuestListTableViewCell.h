7576694e4e7574c6c3c1d8f1602a1e962c3adad6
Максим Шульга <sunshine@zeouniversity.com>
2016-12-14 21:10:09 +0200

    TableCell for simple quest

--- RPG Game/RPGQuestListTableViewCell.h
+++ RPG Game/RPGQuestListTableViewCell.h
@@ -2,26 +2,30 @@
 //  RPGQuestListTableViewCell.h
 //  RPG Game
 //
-//  Created by Максим Шульга on 10/11/16.
+//  Created by Максим Шульга on 12/14/16.
 //  Copyright © 2016 RPG-team. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+  // Constants
+#import "RPGQuestAction.h"
+#import "RPGQuestListState.h"
+#import "RPGQuestEnum.h"
 
-@class RPGQuest;
-@class RPGQuestTableViewController;
+@protocol RPGQuestTableViewCellDelegate <NSObject>
 
-@protocol RPGTableViewCellDelegate <NSObject>
-
-- (void)deleteQuestWithID:(NSInteger)aQuestID;
 - (void)getRewardForQuestWithID:(NSInteger)aQuestID;
+- (void)doQuestAction:(RPGQuestAction)anAction
+               byType:(RPGQuestType)aType
+              questID:(NSInteger)aQuestID
+             friendID:(NSInteger)aFriendID;
 
 @end
 
 @interface RPGQuestListTableViewCell : UITableViewCell
 
-@property (nonatomic, assign, readwrite) id<RPGTableViewCellDelegate> delegate;
+@property (nonatomic, assign, readwrite) id<RPGQuestTableViewCellDelegate> delegate;
 
-- (void)setCellContent:(RPGQuest *)aCellContent;
+- (void)setCellContent:(id)aCellContent;
 
 @end
