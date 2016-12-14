bf347712e370d965f7e2772232868e7acbe4a568
Владислав Крут <pantera@zeouniversity.com>
2016-12-14 19:26:00 +0200

    create UIViewController category for handling

---
+++ RPG Game/UIViewController+RPGWrongTokenHandling.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController + (RPGWrongTokenHandling).h
+//  RPG Game
+//
+//  Created by Владислав Крут on 12/14/16.
+//  Copyright © 2016 RPG-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (RPGWrongTokenHandling)
+
+- (void)handleWrongTokenError;
+
+@end
