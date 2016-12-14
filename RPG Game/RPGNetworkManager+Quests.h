7a809a8bb69065c5ab4a83e65e50f7b6f114386f
Максим Шульга <sunshine@zeouniversity.com>
2016-12-14 21:31:56 +0200

    Update network manager quests with duel quests

--- RPG Game/RPGNetworkManager+Quests.h
+++ RPG Game/RPGNetworkManager+Quests.h
@@ -10,24 +10,30 @@
   // Constants
 #import "RPGQuestListState.h"
 #import "RPGQuestAction.h"
+#import "RPGQuestEnum.h"
   // Entities
 @class RPGQuestRequest;
+@class RPGDuelQuestRequest;
 @class RPGQuestReviewRequest;
 @class RPGQuestListResponse;
+@class RPGIncomingDuelQuestListResponse;
 
 @interface RPGNetworkManager (Quests)
 
-- (void)fetchQuestsByState:(RPGQuestListState)aState
-         completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode,
-                                     RPGQuestListResponse *aResponse))aCallback;
+- (void)fetchQuestsByType:(RPGQuestType)aType
+                    state:(RPGQuestListState)aState
+        completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode,
+                                    RPGQuestListResponse *aResponse))aCallback;
 
 - (void)doQuestAction:(RPGQuestAction)anAction
+               byType:(RPGQuestType)aType
               request:(RPGQuestRequest *)aRequest
     completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode))aCallback;
 
-- (void)addProofWithRequest:(RPGQuestRequest *)aRequest
-                  imageData:(NSData *)anImageData
-          completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode))aCallback;
+- (void)addProofByType:(RPGQuestType)aType
+               request:(RPGQuestRequest *)aRequest
+             imageData:(NSData *)anImageData
+     completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode))aCallback;
 
 - (void)postQuestProofWithRequest:(RPGQuestReviewRequest *)aRequest
                 completionHandler:(void (^)(RPGStatusCode aNetworkStatusCode))aCallback;
